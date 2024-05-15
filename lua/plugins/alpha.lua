5d7173cb01c905c03ce5b9792052e42ff81a114e
Gustavo Lima <11997211+guhenrique007@users.noreply.github.com>
2024-05-15 12:00:32 -0300

    customize dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -113,29 +113,26 @@ return {
     end
 
     dashboard.section.header.val = {
-      " █████  ███████ ████████ ██████   ██████",
-      "██   ██ ██         ██    ██   ██ ██    ██",
-      "███████ ███████    ██    ██████  ██    ██",
-      "██   ██      ██    ██    ██   ██ ██    ██",
-      "██   ██ ███████    ██    ██   ██  ██████",
-      " ",
-      "    ███    ██ ██    ██ ██ ███    ███",
-      "    ████   ██ ██    ██ ██ ████  ████",
-      "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-      "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-      "    ██   ████   ████   ██ ██      ██",
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
     }
     dashboard.section.header.opts.hl = "DashboardHeader"
     dashboard.section.footer.opts.hl = "DashboardFooter"
 
     local get_icon = require("astroui").get_icon
     dashboard.section.buttons.val = {
-      dashboard.button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
-      dashboard.button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
-      dashboard.button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
-      dashboard.button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
-      dashboard.button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
       dashboard.button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+      dashboard.button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+      dashboard.button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      dashboard.button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+      -- dashboard.button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+      -- dashboard.button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
     }
 
     dashboard.config.layout = {
