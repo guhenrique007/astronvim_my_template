9c3b1cacb3dd6dc36b5af5cace2e744b9e3618bf
Gustavo Lima <11997211+guhenrique007@users.noreply.github.com>
2024-05-15 11:32:55 -0300

    update copilot chat command

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -3,18 +3,18 @@ return {
   opts = {
     show_help = "yes", -- Show help text for CopilotChatInPlace, default: yes
     debug = false, -- Enable or disable debug mode, the log file will be in ~/.local/state/nvim/CopilotChat.nvim.log
-    disable_extra_info = 'no', -- Disable extra information (e.g: system prompt) in the response.
+    disable_extra_info = "no", -- Disable extra information (e.g: system prompt) in the response.
     prompts = {
       OptimizePrompt = {
         prompt = "/COPILOT_REFACTOR Optimize the selected code to improve performance and readablilty.",
-      }
+      },
     },
-    language = "English" -- Copilot answer language settings when using default prompts. Default language is English.
+    language = "English", -- Copilot answer language settings when using default prompts. Default language is English.
     -- proxy = "socks5://127.0.0.1:3000", -- Proxies requests via https or socks.
     -- temperature = 0.1,
   },
   build = function()
-    vim.notify("Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim.")
+    vim.notify "Please update the remote plugins by running ':UpdateRemotePlugins', then restart Neovim."
   end,
   event = "VeryLazy",
   keys = {
@@ -50,13 +50,13 @@ return {
     },
     {
       "<leader>xo",
-      ":CopilotChatOpen",
+      ":CopilotChatOpen<cr>",
       desc = "CopilotChat - Open chat window",
     },
     {
       "<leader>xO",
       "<cmd>CopilotChatBuffer  /COPILOT_REFACTOR Optimize the selected code to improve performance and readablilty.<cr>",
       desc = "CopilotChat - Optimize the selected code to improve performance and readablilty.",
-    }
+    },
   },
 }
