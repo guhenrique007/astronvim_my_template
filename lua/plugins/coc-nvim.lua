1b48f23548dedb73b7cb3f301867f09a4c618d10
Gustavo Lima <11997211+guhenrique007@users.noreply.github.com>
2024-06-14 11:25:50 -0300

    revert

--- lua/plugins/coc-nvim.lua
+++ lua/plugins/coc-nvim.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 -- Something here is messing ts, jsx identation
 
 return {
